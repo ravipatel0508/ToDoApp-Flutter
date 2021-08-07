b41fa69e79aa9f332e6715ffc5c635292ea52a04
ravipatel0508 <ravikano1997@gmail.com>
2021-08-08 00:10:35 +0530

    second commit

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -12,15 +12,35 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(
-          children: [
-            Text('TO DO App'),
-            SizedBox(height: 10),
-            Row(children:[
-              //TextField(),
-              FloatingActionButton(onPressed: (){},child: Icon(Icons.add),)
-            ]),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('TO DO App'),
+              SizedBox(height: 10),
+              Row(children: [
+                Flexible(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter a search term'),
+                  ),
+                ),
+                FloatingActionButton(
+                  onPressed: () {},
+                  child: Icon(Icons.add),
+                )
+              ]),
+              Container(
+                height: MediaQuery.of(context).size.height,
+                child: ListView.builder(
+                  itemBuilder: (context, index) => ListTile(
+                    title: Text('text...'),
+                    trailing: IconButton(onPressed: (){}, icon: Icon(Icons.edit)),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
